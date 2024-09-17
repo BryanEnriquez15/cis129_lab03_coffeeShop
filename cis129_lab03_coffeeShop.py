e28ec72d50bb79548e1e7382fe4b881c92007433
BryanEnriquez15 <159509904+BryanEnriquez15@users.noreply.github.com>
2024-09-16 23:28:50 -0600

    Update cis129_lab03_coffeeShop.py
    Updated the coffee shop menu. added 2 new pastries being brownies and banana bread.

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -3,6 +3,8 @@
 # Set Price to coffee and muffin and tax rate
 coffee_price = 5.00
 muffin_price = 4.00
+bananabread_price = 2.00
+brownies_price = 3.00
 tax_rate = 0.06
 
 
@@ -10,12 +12,17 @@ tax_rate = 0.06
 print("Welcome To My Coffee and Muffin Shop")
 num_coffees = int(input("Number of coffees bought?"))
 num_muffins = int(input("Number of muffins bought?"))
+num_bananabread = int(input("Number of banana bread bought?"))
+num_brownies = int(input("Number of brownies bought?"))
+
 
 
 #Calculate cost before tax
 coffee_total = num_coffees * coffee_price
 muffin_total = num_muffins * muffin_price
-subtotal = coffee_total + muffin_total
+bananabread_total = num_bananabread * bananabread_price
+brownies_total = num_browines * brownies_price
+subtotal = coffee_total + muffin_total + bananabread_total + brownies_total
 
 
 #Calculate the tax and total                  
@@ -27,5 +34,11 @@ total = subtotal + tax
 print("My Coffee and Muffin Shop Receipt")
 print(f"{num_coffees} Coffee at ${coffee_price:.2f} each: $ {coffee_total:.2f}")
 print(f"{num_muffins} Muffins at ${muffin_price:.2f} each: $ {muffin_total:.2f}")
+print(f"{num_bananabread} Banana Bread at ${bananabread_price:.2f} each $ {bananabread_total:.2f}")
+print(f"{num_brownies} Brownies at ${brownies_price:.2f} each $ {brownies_total:.2f}")
 print("6% tax: $ {tax:.2f}")
 print(f"Total: $ {total:.2f}")
+
+#Thank you message
+print("Thank you for coming and have a good day!")
+
